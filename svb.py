4e4cf290ba8d1fd9e400908ea11682eda6980fc4
saubeneau <simon.aubeneau@gmail.com>
2018-01-30 16:51:43 +0100

    Update svb.py
    nouvelle version

--- svb.py
+++ svb.py
@@ -1,188 +1,141 @@
 
-# SVB
-
-# TODO :
-# 		 - utiliser le corpus de textes utilisé par les chercheurs
-#		 - utiliser les meme variables que vous
-#		 - pour l'instant, j'ai fait l'hypothese que tous les documents ont la meme taille
-#		 - eventuellement, ajouter un critere de convergence ???
-#
-# je fait les modifs ce soir.
-#
-
-K = 6
-alpha_const = 0.5
-beta_const = 1.
-nb_iter = 100
-
 import pandas as pd
 import numpy as np
 import math
 import scipy.special as ss
 import random
+import time
 
-#
-# creation d'un mini-corpus de documents
-#
 
-documents_list = []
 
-documents_list.append( pd.Series(["antilope","mouton","chat","chien","cheval","chat"]))
-documents_list.append( pd.Series(["antilope","lion","girafe","chien","cheval","chat"]))
-documents_list.append( pd.Series(["boeuf","lion","coq","chien","cochon","chat"]))
-documents_list.append( pd.Series(["antilope","boeuf","chien","chien","cheval","mouton"]))
+docword_KOS = pd.read_csv('docword.kos.txt', sep=" ")
+vocab_KOS= pd.read_csv('vocab.kos.txt', sep=" ")
 
-documents_list.append( pd.Series(["main","tete","oreille","jambe","bras","ventre"]))
-documents_list.append( pd.Series(["main","tete","oreille","tete","doigt","bras"]))
+# corpus doit etre un data frame de 3 colonnes avec:
+#  colonne 1 : id du document
+#  colonne 2 : id du mot
+#  colonne 3 : nbr d'occurence du mot dans le document
 
-documents_list.append( pd.Series(["voiture","camion","bateau","avion","helicoptere","moto"]))
-documents_list.append( pd.Series(["voiture","camion","bateau","voiture","velo","moto"]))
-documents_list.append( pd.Series(["marche","moto","helicoptere","voiture","velo","voiture"]))
+# vocabulaire doit etre un dataframe avec 1 seule colonne:
+#  tq 1+indice(mot)=id du mot
+def svb( corpus, vocabulaire, nbr_classes, nbr_iterations, alpha, beta)
 
-documents_list.append( pd.Series(["france","angleterre","italie","egypte","mali","nigeria"]))
-documents_list.append( pd.Series(["russie","france","italie","egypte","usa","france"]))
-documents_list.append( pd.Series(["russie","angleterre","italie","egypte","usa","nigeria"]))
-documents_list.append( pd.Series(["russie","usa","espagne","france","usa","nigeria"]))
+	K = nbr_classes
+	alpha_const = alpha
+	beta_const = beta
+	nb_iter = nbr_iterations
+	
+	nb_lines = corpus.shape[0]
+	nb_documents = corpus.iloc[nb_lines-1,0]
+	nb_words = vocabulaire.shape[0]
 
-#documents_list.append( pd.Series(["france","chef","cuisine","mouton","boeuf","sauce"]))
-#documents_list.append( pd.Series(["sauce","chef","cuisine","assiette","poivre","sauce"]))
-#documents_list.append( pd.Series(["sauce","france","boeuf","assiette","poivre","fromage"]))
-#documents_list.append( pd.Series(["vin","sauce","coq","assiette","sel","poivre"]))
+	documents_list = []
+	line = 0
+	nb_words_by_document = pd.Series(0, index=np.arange(nb_documents))
+	documents_df = pd.DataFrame(index=np.arange(1000), columns=np.arange(nb_documents))
+	index_words = dict((k,[]) for k in np.arange(nb_words))
+	for i in range(0,nb_lines):
+		no_document = corpus.iloc[i,0]-1
+		for j in range(0,corpus.iloc[i,2]):
+			no_word = corpus.iloc[i,1]-1
+			documents_df.at[nb_words_by_document[no_document],no_document] = no_word
+			list_index = index_words.get(no_word)
+			list_index.append((no_document,nb_words_by_document[no_document]))
+			index_words[no_word] = list_index
+			nb_words_by_document[no_document] += 1
+	#	if( i%10000 == 0 ):
+	#		print(i)
 
-#documents_list.append( pd.Series(["but","gazon","tir","football","gardien","ballon"]))
-#documents_list.append( pd.Series(["but","tir","football","football","attaquant","ballon"]))
+	max_words_by_document = max(nb_words_by_document)
 
-#documents_list.append( pd.Series(["antilope","lion","girafe","egypte","mali","afrique"]))
-#documents_list.append( pd.Series(["tir","france","italie","football","angleterre","ballon"]))
+	gamma = pd.DataFrame(0.,  
+						  index=np.arange(max_words_by_document*K),
+						  columns=np.arange(nb_documents))
+	for d in range(0,nb_documents):
+		for s in range(0,nb_words_by_document[d]*K):
+			gamma.at[s, d] = 1/K + random.gauss(0,1)
 
-documents_list.append( pd.Series(["football","tennis","equitation","basket","aviron","boxe"]))
-documents_list.append( pd.Series(["football","tennis","football","velo","pingpong","course"]))
-documents_list.append( pd.Series(["basket","aviron","equitation","boxe","boxe","course"]))
+	alpha = pd.DataFrame(0.,  
+						  index=np.arange(K),
+						  columns=np.arange(nb_documents))
+	for d in range(0,nb_documents):
+		for i in range(0,K):
+			alpha.at[i, d] = alpha_const
 
-documents_list.append( pd.Series(["crayon","feuille","stylo","imprimante","gomme","crayon"]))
-documents_list.append( pd.Series(["trousse","scotch","stylo","crayon","gomme","feuille"]))
+	beta = pd.DataFrame(0.,
+						  index=np.arange(nb_words),  
+						  columns=np.arange(K))
+	for i in range(0,K):
+		for j in range(0,nb_words):
+			beta.at[j, i] = beta_const
 
-documents_df = pd.DataFrame(documents_list).transpose()
-nb_words_by_document = documents_df.shape[0]
-nb_documents = documents_df.shape[1]
+	# corps de l'algo svb
+	#
 
-#
-# creation de la liste de mots
-#
+	for c in range(0,nb_iter):
+	#	print( c )
+	#	print( " boucle 1 ")
+		for j in range(0,nb_documents):
+			for k in range(0,K):
+				somme = 0
+				for i in range(0,nb_words_by_document[j]):
+					s=i*K+k
+					somme += gamma.iloc[s,j]
+				alpha.at[k,j]= alpha_const + max( 0., somme)
+	#	print( " boucle 2 ")
+		for k in range(0,K):
+			for w in range(0,nb_words):
+				somme = 0
+				for t in index_words.get(w):
+					j = t[0]
+					i = t[1]
+					s=i*K+k
+					somme += gamma.iloc[s,j]
+	#            for j in range(0,nb_documents):
+	#                for i in range(0,nb_words_by_document[j]):
+	#                    if( documents_df.iloc[i,j]==w):
+	#                        s=i*K+k
+	#                        somme += gamma.iloc[s,j]
+				beta.at[w,k]= beta_const + max( 0., somme)
+	#	print( " boucle 3 ")
+		for j in range(0,nb_documents):
+			for i in range(0,nb_words_by_document[j]):
+				somme_gamma = 0
+				for k in range(0,K):
+					t1 = ss.digamma(alpha.iloc[k,j])
+					w = documents_df.iloc[i,j]
+					t2 = ss.digamma(beta.iloc[w,k])
+					somme = beta[k].sum()
+	#                for w in range(0,nb_words):
+	#                    somme += beta.iloc[k,w]
+					t3 = ss.digamma(somme)
+					s=i*K+k
+					gamma.at[s,j]=math.exp(t1+t2-t3)
+					somme_gamma += gamma.at[s,j]
+				for k in range(0,K):
+					s=i*K+k
+					gamma.at[s,j]=gamma.iloc[s,j]/somme_gamma
 
-words_set = set()
-for i in range(0, nb_documents ):
-    for j in range(0, nb_words_by_document):
-        words_set.add(documents_df.iloc[j,i])
-words_list = list(words_set)
-words_df = pd.Series(words_list)
+	return ( alpha, beta, gamma)
 
-nb_words = len(words_set)
-
-mot = pd.DataFrame(0,  
-                      index=np.arange(nb_words_by_document),
-                      columns=np.arange(nb_documents))
-for d in range(0,nb_documents):
-    for n in range(0,nb_words_by_document):
-        mot.at[n, d] = words_list.index(documents_df.at[n, d])
-        
-#
-# calcul des frequences des mots
-# (non-utlise dans cette version de l'algo)
-#
-
-frequence = pd.DataFrame(0.,  
-                      index=np.arange(nb_words),
-                      columns=np.arange(nb_documents))
-for d in range(0,nb_documents):
-    for j in range(0,nb_words):
-        somme = 0
-        for n in range(0,nb_words_by_document):
-            if( documents_df.iloc[n,d] == words_df[j]):
-                somme+=1
-        frequence.at[j, d] = somme / nb_words_by_document
-
-		
-#
-# initialisations des matrices alpha, beta et gamma
-#
-
-gamma = pd.DataFrame(0.,  
-                      index=np.arange(nb_words_by_document*K),
-                      columns=np.arange(nb_documents))
-for d in range(0,nb_documents):
-    for s in range(0,nb_words_by_document*K):
-        gamma.at[s, d] = 1/K + random.gauss(0,1)
-
-alpha = pd.DataFrame(0.,  
-                      index=np.arange(K),
-                      columns=np.arange(nb_documents))
-for d in range(0,nb_documents):
-    for i in range(0,K):
-        alpha.at[i, d] = alpha_const
-
-beta = pd.DataFrame(0.,  
-                      index=np.arange(K),
-                      columns=np.arange(nb_words))
-for i in range(0,K):
-    for j in range(0,nb_words):
-        beta.at[i, j] = beta_const
-
-#
-# corps de l'algo svb
-#
-
-for c in range(0,nb_iter):
-    for j in range(0,nb_documents):
-        for k in range(0,K):
-            somme = 0
-            for i in range(0,nb_words_by_document):
-                s=i*K+k
-                somme += gamma.iloc[s,j]
-            alpha.at[k,j]= alpha_const + somme
-    for k in range(0,K):
-        for w in range(0,nb_words):
-            somme = 0
-            for i in range(0,nb_words_by_document):
-                for j in range(0,nb_documents):
-                    if( documents_df.iloc[i,j]==words_df[w]):
-                        s=i*K+k
-                        somme += gamma.iloc[s,j]
-            beta.at[k,w]= beta_const + somme
-    for i in range(0,nb_words_by_document):
-        for j in range(0,nb_documents):
-            somme_gamma = 0
-            for k in range(0,K):
-                t1 = ss.digamma(alpha.iloc[k,j])
-                w = mot.iloc[i,j]
-                t2 = ss.digamma(beta.iloc[k,w])
-                somme = 0
-                for w in range(0,nb_words):
-                    somme += beta.iloc[k,w]
-                t3 = ss.digamma(somme)
-                s=i*K+k
-                gamma.at[s,j]=math.exp(t1+t2-t3)
-                somme_gamma += gamma.at[s,j]
-            for k in range(0,K):
-                s=i*K+k
-                gamma.at[s,j]=gamma.iloc[s,j]/somme_gamma
-
+triplet = svb( docword_KOS, vocab_KOS, 10, 10, 0.5, 0.5)
 #
 # calcul des topics pour visualiser l'efficacite de l'algo
 #
-
+beta = triplet[1]
+beta2 = beta.transpose()
 phi = pd.DataFrame(0.,  
-                      index=np.arange(K),
-                      columns=np.arange(nb_words))
+					  index=np.arange(K),
+					  columns=np.arange(nb_words))
 for w in range(0,nb_words):
-    dirichletParam = beta[w]
-    distr = np.random.dirichlet( dirichletParam )
-    phi[w] = distr
-    
+	dirichletParam = beta2[w]
+	distr = np.random.dirichlet( dirichletParam )
+	phi[w] = distr
+	
 topic_list = []
 for k in range(0,K):
-    topic = []
-    for w in range(0,nb_words):
-        if( phi.iloc[k,w] == max(phi[w])):
-            topic.append(words_df[w])
-    topic_list.append(topic)
+	topic = []
+	for w in range(0,nb_words):
+		if( phi.iloc[k,w] == max(phi[w])):
+			topic.append(vocabulaire.iloc[w,0])
+	topic_list.append(topic)
